137665997ba1e393cfd1fca740d2b86fd28f81a3
DargyLobo <dargijuli@gmail.com>
2022-03-24 15:29:08 -0500

    Inicio del sistema de gestion

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+require 'conexion.php';
+
+$db = new Database();
+$db->conectar();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=}, initial-scale=1.0">
+    <title>GESTION</title>
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/estilos.css">
+    <script src="public/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<body class="py-3">
+    <main class="container contenedor">
+        <div class="p-3 rounded">
+            <div class="row">
+                <div class="col-12">
+                    <h4>conexion exitosa
+                    </h4>
+                </div>
+            </div>
+
+
+        </div>
+    </main>
+    
+</body>
+</html>
